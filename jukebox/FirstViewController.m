c8b6e83677a1409098c021e66bbf0f0df81bbcdf
atohsuiko <atohsuiko@gmail.com>
2014-12-20 23:08:05 -0800

    search capability

--- jukebox/FirstViewController.m
+++ jukebox/FirstViewController.m
@@ -8,17 +8,71 @@
 
 #import "FirstViewController.h"
 
-@interface FirstViewController ()
-
+@interface FirstViewController () <UISearchDisplayDelegate>
+@property (nonatomic, strong) NSArray *tableData;
+@property (nonatomic, strong) NSMutableArray *searchResult;
 @end
 
 @implementation FirstViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    self.tableData = @[@"One", @"Two", @"Three", @"Four"];
+    self.searchResult = [NSMutableArray arrayWithCapacity:[self.tableData count]];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope
+{
+    [self.searchResult removeAllObjects];
+    NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"SELF contains[c] %@", searchText];
+    
+    self.searchResult = [NSMutableArray arrayWithArray: [self.tableData filteredArrayUsingPredicate:resultPredicate]];
+}
+
+-(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+{
+    [self filterContentForSearchText:searchString scope:[[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:[self.searchDisplayController.searchBar selectedScopeButtonIndex]]];
+    
+    return YES;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (tableView == self.searchDisplayController.searchResultsTableView)
+    {
+        return [self.searchResult count];
+    }
+    else
+    {
+        return [self.tableData count];
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    if (tableView == self.searchDisplayController.searchResultsTableView)
+    {
+        cell.textLabel.text = [self.searchResult objectAtIndex:indexPath.row];
+    }
+    else
+    {
+        cell.textLabel.text = self.tableData[indexPath.row];
+    }
+    
+    return cell;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
--- jukebox/SecondViewController.m
+++ jukebox/SecondViewController.m
@@ -9,14 +9,12 @@
 #import "SecondViewController.h"
 
 @interface SecondViewController ()
-
 @end
 
 @implementation SecondViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view,
 }
 
 - (void)didReceiveMemoryWarning {
