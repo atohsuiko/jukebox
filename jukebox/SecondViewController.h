239fef311c5821fc54d247a2a5e8b5d1684eda8f
atohsuiko <atohsuiko@gmail.com>
2014-12-20 16:06:35 -0800

    Initial Commit

---
+++ jukebox/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  jukebox
+//
+//  Created by Shota  on 12/20/14.
+//  Copyright (c) 2014 ftwave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
