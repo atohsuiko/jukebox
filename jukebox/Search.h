5bda85164c335120b62ceb7a578011d13e00a3ab
atohsuiko <atohsuiko@gmail.com>
2014-12-22 19:46:25 -0800

    post request

---
+++ jukebox/Search.h
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.h
+//  jukebox
+//
+//  Created by Shota  on 12/20/14.
+//  Copyright (c) 2014 ftwave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController<NSURLConnectionDelegate,UISearchDisplayDelegate>
+{
+    NSMutableData *_responseData;
+}
+@property (nonatomic, strong) NSArray *tableData;
+@property (nonatomic, strong) NSMutableArray *searchResult;
+@end
--- jukebox/Search.m
+++ jukebox/Search.m
@@ -6,11 +6,9 @@
 //  Copyright (c) 2014 ftwave. All rights reserved.
 //
 
-#import "FirstViewController.h"
+#import "Search.h"
 
-@interface FirstViewController () <UISearchDisplayDelegate>
-@property (nonatomic, strong) NSArray *tableData;
-@property (nonatomic, strong) NSMutableArray *searchResult;
+@interface FirstViewController ()
 @end
 
 @implementation FirstViewController
@@ -22,6 +20,26 @@
 
     self.tableData = @[@"One", @"Two", @"Three", @"Four"];
     self.searchResult = [NSMutableArray arrayWithCapacity:[self.tableData count]];
+    
+    //POST request
+    // Create the request.
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://google.com"]];
+    
+    // Specify that it will be a POST request
+    request.HTTPMethod = @"POST";
+    
+    // This is how we set header fields
+    [request setValue:@"application/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    // Convert your data and set your request's HTTPBody property
+    NSString *stringData = @"some data";
+    NSData *requestBodyData = [stringData dataUsingEncoding:NSUTF8StringEncoding];
+    request.HTTPBody = requestBodyData;
+    
+    // Create url connection and fire request
+    NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    
+    NSLog(_responseData);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -73,6 +91,43 @@
     
     return cell;
 }
+
+//NSURL methods
+#pragma mark NSURLConnection Delegate Methods
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    // A response has been received, this is where we initialize the instance var you created
+    // so that we can append data to it in the didReceiveData method
+    // Furthermore, this method is called each time there is a redirect so reinitializing it
+    // also serves to clear it
+    _responseData = [[NSMutableData alloc] init];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    // Append the new data to the instance variable you declared
+    [_responseData appendData:data];
+}
+
+- (NSCachedURLResponse *)connection:(NSURLConnection *)connection
+                  willCacheResponse:(NSCachedURLResponse*)cachedResponse {
+    // Return nil to indicate not necessary to store a cached response for this connection
+    return nil;
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    // The request is complete and data has been received
+    // You can parse the stuff in your instance variable now
+    
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    // The request has failed for some reason!
+    // Check the error var
+}
+
+
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
